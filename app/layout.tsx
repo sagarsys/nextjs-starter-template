77de1046c84d4d940a4ffa70f09c3bf2339dfb35
sagar@io <sagar.sawuck@iodigital.com>
2025-02-27 21:33:31 +0100

    generate create react app project and cleanup

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { geistMono, geistSans } from '@/config/fonts';
+import { ReactNode } from 'react';
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
