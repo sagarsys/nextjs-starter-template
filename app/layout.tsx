8a56bee68f99c35084049a4e3c39a6fb0e1f663e
sagar@io <sagar.sawuck@iodigital.com>
2025-02-27 23:47:32 +0100

    add next theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,23 @@
 import '@/styles/globals.css';
 import { geistMono, geistSans } from '@/config/fonts';
 import { ReactNode } from 'react';
+import { ThemeProvider } from '@/components/theme-provider';
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+export default function RootLayout({ children }: Readonly<{ children: ReactNode; }>) {
+    return (
+        <html lang="en" suppressHydrationWarning>
+        <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+        >
+            {children}
+        </ThemeProvider>
+        </body>
+        </html>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@ import { Button } from '@/components/ui/button';
 
 export default function Home() {
     return (
-        <div className="grid">
-            <h1 className="text-3xl font-bold underline">
+        <div className="h-[100vh] grid grid-flow-row grid-cols-1 gap-1 justify-items-center content-center">
+            <h1 className="text-3xl font-bold">
                 Hello world!
             </h1>
-            <Button variant="outline">Click me</Button>
+            <Button className="mt-4 max-w-[500px]">I am a button</Button>
         </div>
     );
 }
