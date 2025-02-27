9e0348abe5d6af97552b0ff13e43c06dd98a2a9c
sagar@io <sagar.sawuck@iodigital.com>
2025-02-27 23:29:43 +0100

    add @shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
-  return (
-      <div>
-          <h1 className="text-3xl font-bold underline">
-              Hello world!
-          </h1>
-      </div>
-  );
+    return (
+        <div className="grid">
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+            <Button variant="outline">Click me</Button>
+        </div>
+    );
 }
